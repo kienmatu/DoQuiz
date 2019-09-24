a5dec3d95ba9263e59e8827cc6acce882f628f05
Kiên Đinh <kienmatu1@gmail.com>
2019-09-24 14:08:37 +0700

    ignorefile

--- TracNghiem/Models/QuizTest.cs
+++ TracNghiem/Models/QuizTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,30 @@ namespace TracNghiem.Models
 {
     public class QuizTest
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public QuizTest()
+        {
+
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string name { get; set; }
+
+        [ForeignKey("Creator")]
+        [Required]
+        public int CreatorID { get; set; }
+
+        public virtual User Creator { get; set; }
+
+        [ForeignKey("Subject")]
+        public int SubjectID { get; set; }
+
+        public virtual Subject Subject { get; set; }
+
+        [Required]
+        public int status { get; set; }
     }
 }
