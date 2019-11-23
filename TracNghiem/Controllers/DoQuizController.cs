d427fc5e96143a38b30b2d66cfa126d3bf82ba63
Kiên Đinh <kienmatu1@gmail.com>
2019-11-23 14:27:20 +0700

    Thêm quizdo

---
+++ TracNghiem/Controllers/DoQuizController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TracNghiem.Models;
+
+namespace TracNghiem.Controllers
+{
+    [Authorize]
+    public class DoQuizController : Controller
+    {
+        QuizContext db = new QuizContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "teacher")]
+        public ViewResult MyActiveTest()
+        {
+            List<ActiveTest> activeTests = db.ActiveTests.ToList();
+            return View(activeTests);
+        }
+    }
+}
